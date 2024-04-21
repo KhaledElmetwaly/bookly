2c9d96a829df63715beb68eba73a14bd5cb09603
khaled Elmetwaly Hegazy <khaled.elmetwaly321@gmail.com>
2024-04-21 16:17:29 +0200

    Add HomeView and navigation logic in SplashViewBody
    Initialize HomeView and add navigation logic to transition to it after a delay
    in SplashViewBody. Also, set the theme background color in GetMaterialApp.

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class Bookly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
       home: const SplashView(),
     );
