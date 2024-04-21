e3837cd92b2e3a4e06a3965f18791cfa82b3d7e4
khaled Elmetwaly Hegazy <khaled.elmetwaly321@gmail.com>
2024-04-21 11:17:43 +0200

    Add initial project files and configurations
    This commit adds initial project files and configurations including settings, constants, assets, and splash screen view components.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/constants.dart';
 import 'package:bookly_app/features/splash/presentation/view/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
@@ -5,13 +6,15 @@ import 'package:get/route_manager.dart';
 void main() {
   runApp(const Bookly());
 }
+
 class Bookly extends StatelessWidget {
   const Bookly({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: SplashView(),
+    return GetMaterialApp(
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      home: const SplashView(),
     );
   }
 }
