64f577fd2a5b101a4c97fda4419225094e6ec119
khaled Elmetwaly Hegazy <khaled.elmetwaly321@gmail.com>
2024-04-28 11:39:40 +0300

    Refactor fetchBNewestBooks to fetchNewestBooks

--- lib/features/home/use_cases/fetch_newest_books_use_case.dart
+++ lib/features/home/use_cases/fetch_newest_books_use_case.dart
@@ -1,21 +1,19 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:bookly_app/core/errors/failure.dart';
-import 'package:bookly_app/features/home/domain_layer/entities/book_entity.dart';
 import 'package:bookly_app/features/home/domain_layer/repositories/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../core/use_cases/use_case.dart';
+import '../domain_layer/entities/book_entity.dart';
 
-class FetchFeturedBookUseCase extends UseCase<List<BookEntity>,NoParam> {
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
-  FetchFeturedBookUseCase(
+  FetchNewestBooksUseCase(
     this.homeRepo,
   );
-  
+
   @override
-  Future<Either<Failure, List<BookEntity>>> call([NoParam?param]) async{
-return await homeRepo.fetchFeaturedBooks(); 
- }
-
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchNewestBooks();
+  }
 }
-
