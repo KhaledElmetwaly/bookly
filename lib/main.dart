36eefc64271e47c91ed208350d19693f6c0bb63d
khaled Elmetwaly Hegazy <khaled.elmetwaly321@gmail.com>
2024-04-29 14:04:28 +0300

    register adapter and open featured box

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:bookly_app/core/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/features/home/domain_layer/entities/book_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main()async {
   runApp(const Bookly());
+  Hive.registerAdapter(BookEntityAdapter());
+await  Hive.openBox(kFeaturedBox);
+
 }
 
 class Bookly extends StatelessWidget {
@@ -13,7 +18,6 @@ class Bookly extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: AppRouter.router,
-
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
