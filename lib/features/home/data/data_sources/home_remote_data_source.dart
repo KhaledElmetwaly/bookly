43f1de9eb54c1109abf1e10197ae39d76c43359d
khaled Elmetwaly Hegazy <khaled.elmetwaly321@gmail.com>
2024-04-29 12:49:48 +0300

    implement fetch newest books

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,40 +1,43 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
 
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/models/book_model.dart';
 
 import '../../domain_layer/entities/book_entity.dart';
 
-abstract class HomeRemoteDataSource{
-   Future< List<BookEntity>> fetchFeaturedBooks();
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
   Future<List<BookEntity>> fetchNewestBooks();
 }
+
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   final ApiService apiService;
   HomeRemoteDataSourceImpl({
     required this.apiService,
   });
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks()async {
-  var data=await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&q=programming');
-  List<BookEntity> books = getBooksList(data);
-  return books;
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=programming');
+    List<BookEntity> books = getBooksList(data);
+    return books;
   }
 
   List<BookEntity> getBooksList(Map<String, dynamic> data) {
-     List<BookEntity>books=[];
-    
-    for(var bookMap in data['items']){
+    List<BookEntity> books = [];
+
+    for (var bookMap in data['items']) {
       books.add(BookModel.fromJson(bookMap));
     }
+
     return books;
   }
 
   @override
-  Future<List<BookEntity>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchNewestBooks() async {
+    var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=programming&sorting=newest');
+    List<BookEntity> books = getBooksList(data);
+    return books;
   }
-
 }
