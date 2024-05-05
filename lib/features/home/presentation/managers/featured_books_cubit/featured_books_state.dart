a05c85b050ffd804679dc879a31f5b29930f07e1
KhaledElmetwaly <khaled.elmetwaly321@gmail.com>
2024-05-05 13:30:11 +0300

    create featured books states

---
+++ lib/features/home/presentation/managers/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'featured_books_cubit.dart';
+
+abstract class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+class FeaturedBooksLoading extends FeaturedBooksState {}
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errorMessage;
+  const FeaturedBooksFailure(
+  this.errorMessage,
+  );
+}
+class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookEntity> books;
+  const FeaturedBooksSuccess(this.books);
+}
