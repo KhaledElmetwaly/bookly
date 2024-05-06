4be8be24a3772d8c8ce94d1a641ab92091737669
KhaledElmetwaly <khaled.elmetwaly321@gmail.com>
2024-05-06 14:35:38 +0300

    give type to hive box of book entity

--- lib/core/utils/functions/save_box.dart
+++ lib/core/utils/functions/save_box.dart
@@ -3,6 +3,7 @@ import 'package:hive_flutter/adapters.dart';
 import '../../../features/home/domain_layer/entities/book_entity.dart';
 
 void saveBoxData(List<BookEntity> books, String boxName) {
-  var box = Hive.box(boxName);
+  var box = Hive.box<BookEntity>(boxName);
+
   box.addAll(books);
 }
