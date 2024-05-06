2ccb36e14f292fbe9b3d1ec469e2d0a74b96942a
KhaledElmetwaly <khaled.elmetwaly321@gmail.com>
2024-05-06 14:37:06 +0300

    show data from server on presentation

--- lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,28 +1,26 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../../core/utils/assets.dart';
-
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({super.key});
+  final String image;
+  const CustomBookImage({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.sizeOf(context).height * .23,
       child: AspectRatio(
-        aspectRatio: 2.8 / 4,
-        child: Container(
-          width: 100,
-          decoration: BoxDecoration(
+          aspectRatio: 2.8 / 4,
+          child: ClipRRect(
             borderRadius: BorderRadius.circular(16),
-            color: Colors.red,
-            image: const DecorationImage(
+            child: CachedNetworkImage(
               fit: BoxFit.fill,
-              image: AssetImage(AssetsData.joker),
+              imageUrl: image,
             ),
-          ),
-        ),
-      ),
+          )),
     );
   }
 }
--- lib/features/home/presentation/views/widgets/featured_listview.dart
+++ lib/features/home/presentation/views/widgets/featured_listview.dart
@@ -1,19 +1,27 @@
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-class FeaturesBookListView extends StatelessWidget {
-  const FeaturesBookListView({super.key});
+import 'package:bookly_app/features/home/domain_layer/entities/book_entity.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 
+class FeaturesBookListView extends StatelessWidget {
+  const FeaturesBookListView({
+    Key? key,
+    required this.books,
+  }) : super(key: key);
+  final List<BookEntity> books;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.sizeOf(context).height * .23,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 5,
-        itemBuilder: (context, index) => const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: CustomBookImage(),
+        itemCount: books.length,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CustomBookImage(
+            image: books[index].image,
+          ),
         ),
       ),
     );
